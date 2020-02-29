05a4f4fce6b7e44aa0288d32a5719b7b9e385f4a
Emilio Rubiales <vintajeskull98@gmail.com>
2020-02-29 19:09:53 +0100

    Login Form

--- src/delphosclient/DelphosClient.java
+++ src/delphosclient/DelphosClient.java
@@ -5,6 +5,8 @@
  */
 package delphosclient;
 
+import Client.Login;
+
 /**
  *
  * @author vinta
@@ -16,6 +18,8 @@ public class DelphosClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
